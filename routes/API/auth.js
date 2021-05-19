d339a36f3a0708345663a26110ef5e33ef6a3fe8
Farouk <faroukkisuule55@gmail.com>
2021-05-18 22:21:04 -0400

    Added login route for verified users

--- routes/API/auth.js
+++ routes/API/auth.js
@@ -1,15 +1,67 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/user");
-const auth = require("../../middleware/auth")
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
 
-router.get("/", auth, async (req,res)=>{
-   try{
+router.get("/", auth, async (req, res) => {
+  try {
     const user = await User.findById(req.user.id).select("-password");
-    res.json(user)
-   } catch(err){
-res.status(500).json("Server Error")
-   }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json("Server Error");
+  }
 });
+
+router.post(
+  "/",
+  [
+    check("email", "Please use valid email on file").isEmail(),
+    check(
+      "password",
+      "Use valid Password associated with account with 6 or more characters"
+    ).exists(),
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        res.status(400).send({ errors: [{ msg: "Invalid User Information" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .send({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: "5000000" },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      res.status(500).json("Server Error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,24 +5,19 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 connectDB();
 
-app.use("/api/user",require("./routes/API/home"));
-app.use("/api/auth",require("./routes/API/auth"));
-app.use("/api/register",require("./routes/API/register"));
+app.use("/api/user", require("./routes/API/home"));
+app.use("/api/auth", require("./routes/API/auth"));
+app.use("/api/register", require("./routes/API/register"));
 
-app.get("/",(req,res)=>{
-    res.send("This is Pull Up")
+app.get("/", (req, res) => {
+  res.send("This is Pull Up");
 });
 
-app.listen(PORT, ()=>{
-    console.log(" ===> Listening to PORT " + PORT)
+app.listen(PORT, () => {
+  console.log(" ===> Listening to PORT " + PORT);
 });
-
-function getRandom(num) {
-    return Math.floor(Math.random());
-  }
-
